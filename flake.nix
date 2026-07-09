dda23606b1fd4a0816300498bf89d72d391baac7
Hobr <mail@hobr.site>
2026-07-10 00:04:59 +0800

    flake: init

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flakelight.url = "github:nix-community/flakelight";
   };
 
@@ -9,17 +10,49 @@
     flakelight ./. {
       inherit inputs;
 
+      withOverlays = [
+        inputs.rust-overlay.overlays.default
+        (final: prev: {
+          rustToolchain =
+            let
+              rust = prev.rust-bin;
+            in
+            if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.stable."1.96.1".default.override {
+                extensions = [
+                  "rust-src"
+                  "rustfmt"
+                  "rust-analyzer"
+                  "clippy"
+                  "cargo"
+                  "llvm-tools"
+                ];
+              };
+        })
+      ];
+
       devShell =
         pkgs: with pkgs; rec {
           packages = [
+            pkg-config
+            openssl
+            rustToolchain
             pre-commit
-
-            stdenv.cc.cc.lib
           ];
 
           env = {
             LD_LIBRARY_PATH = lib.makeLibraryPath packages;
+            RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
           };
+
+          shellHook = ''
+            export CARGO_HOME="$PWD/.cargo"
+            export PATH="$CARGO_HOME/bin:$PWD/target/release:$PATH"
+          '';
         };
     };
 }
